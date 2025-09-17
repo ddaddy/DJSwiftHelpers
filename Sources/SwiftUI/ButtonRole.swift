f77ad1d6b2379b706866173a6851bb47b84cacde
Darren Jones <1425808+ddaddy@users.noreply.github.com>
2025-09-17 11:38:30 +0200

    [SwiftUI] Added 26.0 ButtonRole's with fallback

---
+++ Sources/SwiftUI/ButtonRole.swift
@@ -0,0 +1,31 @@
+//
+//  ButtonRole.swift
+//  DJSwiftHelpers
+//
+//  Created by Darren Jones on 17/09/2025.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+@available(iOS 15.0, macOS 12.0, watchOS 8.0, *)
+public extension ButtonRole {
+    
+    public static func confirm(fallback: ButtonRole?) -> ButtonRole? {
+        if #available(iOS 26.0, macOS 26.0, watchOS 26.0, *) {
+            return .confirm
+        } else {
+            return fallback
+        }
+    }
+    
+    public static func close(fallback: ButtonRole?) -> ButtonRole? {
+        if #available(iOS 26.0, macOS 26.0, watchOS 26.0, *) {
+            return .close
+        } else {
+            return fallback
+        }
+    }
+}
+
+#endif // canImport(SwiftUI)
