e27a3a48ee328a16e95319960ae3e0a820431803
Darren Jones <1425808+ddaddy@users.noreply.github.com>
2025-10-20 13:24:24 +0200

    [SwiftUI] Improved MailComposer

--- Sources/SwiftUI/MailComposer.swift
+++ Sources/SwiftUI/MailComposer.swift
@@ -10,6 +10,65 @@ import SwiftUI
 import UIKit
 import MessageUI
 
+@available(iOS 15.0, *)
+extension View {
+    
+    /// A view modifier that can present a mail composer view
+    ///
+    /// - Parameter isShowing: A binding to show the email composer
+    /// - Parameter composer: The `MailComposer` to use
+    /// - Parameter onFail: A view builder containing the button to display in the failure sheet
+    ///
+    /// ```swift
+    /// .mailComposerView(isShowing: $isShowingMailComposer, with: Feedback.mailComposer(), onFail: {
+    ///     Button("Share Logs") {
+    ///         shareLogs()
+    ///     }
+    /// })
+    /// ```
+    @MainActor
+    public func mailComposerView<A>(isShowing: Binding<Bool>, with composer: MailComposer, @ViewBuilder onFail: (@escaping () -> A)) -> some View where A: View {
+        modifier(MailComposerView(isShowing: isShowing, with: composer, onFail: onFail))
+    }
+}
+
+@available(iOS 15.0, *)
+@MainActor
+private struct MailComposerView<A>: ViewModifier where A: View {
+    
+    internal init(isShowing: Binding<Bool>, with composer: MailComposer, @ViewBuilder onFail: @escaping () -> A) {
+        self._isShowing = isShowing
+        self.composer = composer
+        self.onFail = onFail
+    }
+    
+    @Binding private var isShowing: Bool
+    private var composer: MailComposer
+    private var onFail: () -> A
+    
+    @State private var showComposer = false
+    @State private var showFailOptions = false
+    
+    func body(content: Content) -> some View {
+        content
+            .sheet(isPresented: $showComposer) {
+                composer
+            }
+            .confirmationDialog("Cannot open iOS Mail!", isPresented: $showFailOptions, titleVisibility: .visible, actions: onFail)
+            .onChange(of: isShowing) { newValue in
+                if newValue == true {
+                    if MFMailComposeViewController.canSendMail() {
+                        showComposer = true
+                    } else {
+                        showFailOptions = true
+                    }
+                    // Reset the external trigger so setting it to true again will re-run this flow
+                    isShowing = false
+                }
+            }
+    }
+}
+
 /// A SwiftUI view to display the iOS Mail Sheet
 ///
 /// - Parameter presentation: A `Binding` for a bool that makes the view appear when switched to `true`
